83d0283f330ed9d725cff5feae623d0532fed055
Tripti Singh <triptisinghh897@gmail.com>
2026-04-29 22:41:44 +0530

    Time: 7 ms (22.95%), Space: 19 MB (99.72%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        wordSet = set(wordDict)  # fast lookup
+        dp = [False] * (len(s) + 1)
+        
+        dp[0] = True
+        
+        for i in range(1, len(s) + 1):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordSet:
+                    dp[i] = True
+                    break
+        
+        return dp[len(s)]
