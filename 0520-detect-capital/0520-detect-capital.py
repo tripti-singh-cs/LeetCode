a834ab62a056b5da93a725aae8ab5aa7492084fb
Tripti Singh <triptisinghh897@gmail.com>
2026-05-24 23:18:09 +0530

    Time: 0 ms (100%), Space: 19.3 MB (62%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.py
@@ -0,0 +1,13 @@
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+
+        if word.isupper():
+            return True
+
+        if word.islower():
+            return True
+
+        if word[0].isupper() and word[1:].islower():
+            return True
+
+        return False
